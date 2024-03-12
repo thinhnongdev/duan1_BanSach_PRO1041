616f75671d8f56b05b169eb11d3c5a6c785bcac2
nam0167 <162852387+nam0167@users.noreply.github.com>
2024-03-12 19:16:13 +0700

    FormVoucher

--- PhanMemBanSach2/src/com/raven/event/EventMenu.java
+++
@@ -1,6 +0,0 @@
-package com.raven.event;
-
-public interface EventMenu {
-
-    public void menuIndexChange(int index);
-}
--- PhanMemBanSach2/src/com/raven/event/EventMenuCallBack.java
+++
@@ -1,6 +0,0 @@
-package com.raven.event;
-
-public interface EventMenuCallBack {
-
-    public void call(int index);
-}
